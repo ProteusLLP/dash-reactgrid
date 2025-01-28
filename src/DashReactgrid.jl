cb459b667ee96dc6a5e524ea00fb919509a48a57
james.norman@proteusllp.com <james.norman@proteusllp.com>
2025-01-28 14:21:41 +0000

    updated version number

--- src/DashReactgrid.jl
+++ src/DashReactgrid.jl
@@ -3,7 +3,7 @@ module DashReactgrid
 using Dash
 
 const resources_path = realpath(joinpath( @__DIR__, "..", "deps"))
-const version = "0.0.1"
+const version = "0.0.19"
 
 include("jl/dashreactgrid.jl")
 
